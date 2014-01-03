7dbf8aeaf33bc42c9d0db956cf4151be9d7bb065
Erik Price <erik.price16@gmail.com>
2014-01-02 23:44:03 -0500

    Flesh out configuration handling a bit more

--- main.go
+++ main.go
@@ -3,37 +3,52 @@ package main
 import (
 	"code.google.com/p/gcfg"
 	"flag"
-	_ "github.com/boredomist/mixport/mixpanel"
-	_ "github.com/boredomist/mixport/streaming"
+	"github.com/boredomist/mixport/mixpanel"
+	"github.com/boredomist/mixport/streaming"
+	"log"
+	"path"
+	"sync"
+	"time"
 )
 
+const defaultConfig = "mixport.conf"
+
+var configFile string
+
+// configFormat is the in-memory representation of the mixport configuration
+// file.
+type configFormat struct {
+	Product map[string]*struct {
+		Key    string
+		Secret string
+		Token  string
+	}
+
+	Kinesis struct {
+		State     bool
+		Keyid     string
+		Secretkey string
+		Stream    string
+		Region    string
+	}
+
+	JSON struct {
+		State     bool
+		Directory string
+	}
+
+	CSV struct {
+		State     bool
+		Directory string
+	}
+}
+
+func init() {
+	const confUsage = "path to configuration file"
+	flag.StringVar(&configFile, "config", defaultConfig, confUsage)
+	flag.StringVar(&configFile, "c", defaultConfig, confUsage+"(shorthand)")
+}
+
 func main() {
-	cfg := struct {
-		Product map[string]*struct {
-			Key    string
-			Secret string
-			Token  string
-		}
-
-		Kinesis struct {
-			keyid     string
-			Secretkey string
-			Stream    string
-			Region    string
-		}
-
-		JSON struct {
-			Directory string
-		}
-
-		Csv struct {
-			Directory string
-		}
-	}{}
-
-	gcfg.ReadFileInto(cfg, "TODO: filename")
-
-	// TODO: write me
-
 	flag.Parse()
 }
